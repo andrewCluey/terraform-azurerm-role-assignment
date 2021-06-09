5761f150034448d4d2d2a62b38bf2328532c8312
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-06-09 17:23:35 +0100

    added description to variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "custom_role_definition_id" {
   type        = string
-  description = "description"
+  description = "If assigning a custom role, enter the ID here. One of either a Custom Role Id or a BuiltIn Role Name must be set."
   default     = null
 }
 
 variable "builtin_role_definition_name" {
     type        = any
-    description = "description"
+    description = "If assigning a built in role, enter the name here. If notusing this parameter, then 'custom_role_defintion_id' must be set. "
     default     = null
 }
 
