601fd77531a46a7ba9b2d31260fe9f5865532146
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-06-09 16:59:37 +0100

    added variabled to example deployment

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -4,5 +4,4 @@ module "role_assignment" {
     scope_id                     = var.subscriptions
     builtin_role_definition_name = "Contributor"
     principal_id                 = var.principal_id
-    
 }
---
+++ examples/simple/variables.tf
@@ -0,0 +1,9 @@
+variable "subscriptions" {
+  type        = string
+  description = "description"
+}
+
+variable "principal_id" {
+  type        = string
+  description = "description"
+}
